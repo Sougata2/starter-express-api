4f8a391a8416322f9a7e2f6df3b308fa99448af0
sougata <sougata19582001@gmail.com>
2024-03-16 23:04:16 +0530

    integrated firebase as database for cities

--- index.js
+++ index.js
@@ -2,32 +2,10 @@ const express = require("express");
 const fs = require("fs-extra");
 const bodyParser = require("body-parser");
 const app = express();
+const { createCity, getCityList, getCityById } = require("./firebase");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-async function readDataFile() {
-  const obj = await fs.readJSON("./data/cities.json");
-  return obj;
-}
-
-async function writeDataFile(writeData, writeObj) {
-  const readData = await readDataFile();
-  // console.log(readData);
-  const id = Math.ceil(Math.random() * 99999999);
-  const newData = { ...writeData, id };
-  const finalData = {
-    cities: Object.values(readData[writeObj]).concat(newData),
-  };
-  console.log(finalData);
-  try {
-    await fs.writeJSON("./data/cities.json", finalData);
-  } catch (err) {
-    console.log(err);
-    return "Opps! something went wrong";
-  }
-  return "City Added";
-}
 
 // Add middleware to enable CORS
 app.use((req, res, next) => {
@@ -46,7 +24,7 @@ app.get("/:name", async (req, res) => {
   console.log("Just got a request!");
   const name = req.params.name;
   if (name !== "cities") return;
-  const data = await readDataFile();
+  const data = await getCityList(name);
   res.json(data[name]);
 });
 
@@ -54,15 +32,15 @@ app.get("/:name/:id", async function (req, res) {
   console.log("Just got a request!");
   const name = req.params.name;
   const id = req.params.id;
-  const data = await readDataFile();
-  res.json(data[name].filter((city) => city.id === +id));
+  const data = await getCityById(id);
+  res.json(data);
 });
 
 app.post("/cities/add", async function (req, res) {
   const data = req.body;
   console.log(data);
-  const message = await writeDataFile(data, "cities");
-  res.send(message);
+  const createId = await createCity(data);
+  res.send(createId);
 });
 
 app.listen(process.env.PORT || 3000);
